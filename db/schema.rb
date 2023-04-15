23d905dcf5256cd63ae04b5f5cbb90dca7b7bf96
Otavio Vasconcelos <otaviocavasc2@gmail.com>
2023-04-15 10:53:35 -0300

    add dotenv

--- db/schema.rb
+++ db/schema.rb
@@ -42,12 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_200929) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "image_processes", force: :cascade do |t|
-    t.string "text_output"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "images", force: :cascade do |t|
     t.string "text_output"
     t.datetime "created_at", null: false
